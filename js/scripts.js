336b605059fbf59ccea2c1105ee924f1fac491d4
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-17 09:38:47 -0700

    add function to check if value is a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
+// Back end
+
+
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Checks if entered value is a number
+function checkIfNumber(array) {
+  if (alpha.includes(array.charAt(0,-1))) {
+    alert("enter a number");
+    Default();
+  } else {
+    return result
+  }
+}
+
+
+
+
+// User Interface
 $(document).ready(function(){
   $("form#submitNum").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("input#num").val();
     console.log(userInput);
-    var split = userInput.split(" ");
-    console.log(split);
 
+    var userInputArr = userInput.split(" ");
+    console.log(userInputArr);
+
+    userInputArr.forEach(function(sendBackEnd) {
+      checkIfNumber(sendBackEnd);
+
+    });
   });
 });
