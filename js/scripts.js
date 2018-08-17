d3743243662588244cdd548811f3556e3819894d
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-17 10:03:56 -0700

    add return of function to number checker as an alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Back end
 
-
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //Checks if entered value is a number
 function checkIfNumber(array) {
@@ -8,7 +7,7 @@ function checkIfNumber(array) {
     alert("enter a number");
     Default();
   } else {
-    return result
+  return alert(array);
   }
 }
 
