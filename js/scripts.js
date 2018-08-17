ca74ad6ef92baa35d9f1337e0e805295c458c627
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-17 08:58:10 -0700

    add split to set all numbers entered into an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   $("form#submitNum").submit(function(event) {
     event.preventDefault();
 
-    var userInput = parseInt($("input#num").val());
+    var userInput = $("input#num").val();
     console.log(userInput);
+    var split = userInput.split(" ");
+    console.log(split);
 
   });
 });
