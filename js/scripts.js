4f9d232d82d9210f15601bc19fa6d0d83faeb617
Catherine Bradley <catherinetybradley@gmail.com>
2018-08-17 12:31:16 -0700

    add return input containing 1 as boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Back end
-
-
+var ifZero = ["0"];
+var ifOne = ["1"];
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //Checks if entered value is a number
 function letterCheck(array) {
@@ -14,11 +14,23 @@ function letterCheck(array) {
 }
 
 function numThreeCheck(array) {
-  var Dave = ("I can't do that")
+  var Dave = ("I can't do that, Dave")
   if (array%3 === 0) {
     return Dave;
   } else {
-    return array;
+    return beepBoop(array);
+  }
+}
+
+function beepBoop(array) {
+  var boop = ("boop");
+  var beep = ("beep");
+  if (ifOne.includes(array.charAt(0,-1))) {
+    return boop;
+  } else if (ifZero.includes(array.charAt(0,-1))) {
+      return beep;
+  } else {
+      return array;
   }
 }
 
