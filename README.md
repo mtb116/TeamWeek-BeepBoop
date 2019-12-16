af1b98ee87c2e0f2e4892adf1cf3d9dd9782dfdf
ctb116 <cathy@epicodus.com>
2019-12-16 15:18:54 -0800

    made beepboop less broken and bad

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
-# _Beep-Boop_
-
-#### _Week 2 Review for Epicodus, 8.17.2018_
-
-#### By _**Catherine Bradley**_
+# _Team Week Beep-Boop_
 
 ## Description
+Fix my broken Beep Boop project!
+Message me on discord to be invited to this repo
 
-_This is an interactive webpage designed to demonstrate my skills using Loops and Arrays with JavaScript and jQuery. I use for() and forEach() to separately process multiple values entered at one time. I also use JavaScript method .include() to check arrays for specific values. Additionally I use personal CSS styling to give the webpage a Space Odyssey theme._
-
-_Specifications are ordered by highest priority first_
 
 ### Specifications
-* _1) Program replaces any integer divisible by 3 with "I'm sorry, Dave. I'm afraid I can't do that." with image of HAL from Space Odyssey_
+* _1) If  input number is divisible by 3, returns "I'm sorry, Dave. I'm afraid I can't do that." with image of HAL from Space Odyssey. Does not return a range of numbers_
   * _**Example Input: 3**_
   * _**Example Output: "I'm sorry, Dave. I'm afraid I can't do that." with image**_
 
@@ -22,22 +17,15 @@ _Specifications are ordered by highest priority first_
   * _**Example Input: 0**_
   * _**Example Output: [beep]**_
 
-  * _**Example Input: 205**_
-  * _**Example Output: [beep]**_
+  * _**Example Input: 10**_
+  * _**Example Output:** [beep], 1, 2, 3, 4, 5, 6, 7, 8, 9, [beep]_
 
 * _3) Program replaces integers containing 1 with "boop"_
   * _**Example Input: 1**_
   * _**Example Output: [boop]**_
 
-  * _**Example Input: 211**_
-  * _**Example Output: [boop]**_
-
-* _4) Else Program returns integer as is"_
-  * _**Example Input: 4**_
-  * _**Example Output: 4**_
-
-  * _**Example Input: 1 4 53 **_
-  * _**Example Output: [boop] 4 53**_
+  * _**Example Input: 11**_
+  * _**Example Output:** [beep], [boop], 2 , 3, 4, 5, 6, 7, 8, 9, [beep], [boop]_
 
 
 ## Setup/Installation Requirements
@@ -45,17 +33,6 @@ _Specifications are ordered by highest priority first_
 * _Clone this repository_
 * _Open the Index.html file in Chrome web browser_
 
-_This webpage will likely work as expected in any web browser but specifically was designed for Chrome._
-
-## Known Bugs
-
-* _If a letters are entered an alert will ask for a number to be inputted and the program will stop. However, if a number is entered first, followed by a space and then letter(s), the program will give the expected alert but continue the program with the first given number._
-
-* _If separate numbers are entered and at least one is divisible by 3 then specification 1 will proceed as usual. However, the other numbers will also appear in the order they were entered._
-
-## Support and contact details
-
-_For feedback, please contact me at catherinetybradley@gmail.com_
 
 ## Technologies Used
 
@@ -65,8 +42,3 @@ _For feedback, please contact me at catherinetybradley@gmail.com_
 * _JavaScript_
 * _jQuery_
 
-### Legal
-
-*This software is licensed under the GPL license.*
-
-Copyright (c) 2018 **_Catherine Bradley_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,50 @@
-// Back end
-var ifZero = ["0"];
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//Checks if entered value is a number
-function letterCheck(array) {
-  if (alpha.includes(array.charAt(0,-1))) {
-    alert("enter a number");
-    Default();
-  } else {
-    console.log(array);
-    return numThreeCheck(array);
-  }
-}
 
-function numThreeCheck(array) {
+//Checks if user input is a number. returns an alert if not.
+//Removed for now.
+
+// var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// function letterCheck(num) {
+//   if (alpha.includes(num.charAt(0, -1))) {
+//     alert("enter a number");
+//     Default();
+//   } else {
+//     return BeepBoop(num);
+//   }
+// }
+
+
+
+//This function just doesn't work!!!!!
+//should at least output a range of numbers from 0 to the user input.
+function BeepBoop(num) {
   var Dave = ("I'm sorry, Dave. I'm afraid I can't do that.")
   var zero = ("beep");
 
-  if (ifZero.includes(array.charAt(0))) {
-    return zero;
-  } else if (array%3 === 0) {
+  console.log(num);
+
+  for (var i = 0; i < num; i++) {
+    console.log(i);
+    if (i = 0) {
+      return zero;
+    } else if (i % 3 === 0) {
       $("#robot").fadeIn();
       return Dave;
-  } else {
-      return beep(array);
-  }
-}
-
-
-function beep(array) {
-  var zero = ("beep");
-  var checkZero = array.split("");
-
-  console.log("array length is " + array.length);
-  console.log("var checkZero is " + checkZero);
-
-  for (i = 0; i < checkZero.length; i++) {
-    if (checkZero.includes("0")) {
-      return zero;
     } else {
-      return boop(array);
+      return i;
     }
   }
 }
 
-function boop(array) {
-  var one = ("boop");
-  var checkOne = array.split("");
 
-  console.log("array at boop function is " + array);
-  console.log("var checkOne is" + checkOne);
 
-  for (i = 0; i < checkOne.length; i++) {
-    if (checkOne.includes("1")) {
-      return one;
-    } else {
-      return array;
-    }
-  }
-}
-
-// User Interface
-$(document).ready(function(){
-  $("form#submitNum").submit(function(event) {
+$(document).ready(function () {
+  $("form#submitNum").submit(function (event) {
     event.preventDefault();
 
-    var userInput = $("input#num").val();
-    console.log("Entered value is " + userInput);
+    var userInput = parseInt($("input#num").val());
+    console.log(userInput);
 
-    var userInputArr = userInput.split(" ");
-    console.log("Array is now " + userInputArr);
+    $("#outputResult").append(" " + BeepBoop(userInput));
 
-    userInputArr.forEach(function(checkletters) {
-      $("#outputResult").append(" " + letterCheck(checkletters));
-    });
   });
 });
