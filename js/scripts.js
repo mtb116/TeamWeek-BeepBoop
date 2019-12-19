5e8b130ae0c852fb97696f6f98adcf19d10c305e
ctb116 <cathy@epicodus.com>
2019-12-18 18:38:24 -0800

    add Jason's letter check

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 
-//Checks if user input is a number. returns an alert if not.
-//Removed for now.
-
-// var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// function letterCheck(num) {
-//   if (alpha.includes(num.charAt(0, -1))) {
-//     alert("enter a number");
-//     Default();
-//   } else {
-//     return BeepBoop(num);
-//   }
-// }
-
-
 //Business Logic
+
+function alphanumeric(number) {
+  var letters = /^[a-z]+$/;
+  if (number.match(letters)) {
+    alert('Please input alphanumeric characters only');
+  } else {
+    return beepBoop(parseInt(number));
+  }
+}
+
 var beepBoop = function (num) {
   let nums = [];
   var Dave = ("I'm sorry, Dave. I'm afraid I can't do that.");
@@ -26,16 +22,16 @@ var beepBoop = function (num) {
     $("#robot").fadeIn();
     return Dave;
   } else {
-  for (i = 0; i <= num; i++) {
-    if (i.toString().includes("0")) {
-      nums.push(zero);
-    } else if (i.toString().includes("1")) {
-      nums.push(one); 
-    } else {
-    nums.push(i);
+    for (i = 0; i <= num; i++) {
+      if (i.toString().includes("0")) {
+        nums.push(zero);
+      } else if (i.toString().includes("1")) {
+        nums.push(one);
+      } else {
+        nums.push(i);
+      }
     }
-  }
-  return nums;
+    return nums;
   }
 };
 
@@ -45,8 +41,8 @@ $(document).ready(function () {
   //Submit Form
   $("form#submitNum").submit(function (event) {
     event.preventDefault();
-    let number = parseInt($("input#num").val());
-    $("#outputResult").text(beepBoop(number));
+    let number = $("input#num").val();
+    $("#outputResult").text(alphanumeric(number));
     $("#outputResult").fadeIn();
   })
 })
